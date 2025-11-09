f8172ce8fed7d539213af8b41d71cf5cead7bce1
lokeshjammi <jammilokesh3@gmail.com>
2025-11-09 22:38:32 +0530

    openai generate text with stream code commit

--- openai_generate_text.py
+++ openai_generate_text.py
@@ -7,22 +7,20 @@ load_dotenv(".env")
 api_key = os.getenv("OPENAI_API_KEY")
 
 if api_key:
-    print("API Key loaded successfully.")
+    print("✅ API Key loaded successfully.\n")
 else:
-    print("Failed to load API Key.")
+    print("❌ Failed to load API Key.")
+    exit()
 
 client = OpenAI(api_key=api_key)
 
-response = client.chat.completions.create(
-    model="gpt-5",
+with client.chat.completions.stream(
+    model="gpt-4o-mini",
     messages=[
         {"role": "system", "content": "You are a helpful assistant."},
         {"role": "user", "content": "Explain the theory of relativity in simple terms."}
-    ],
-    stream=True
-)
-
-# print(response.choices[0].message.content)
-for chunk in response:
-    if chunk.choices[0].message.content:
-        print(chunk.choices[0].message.content, end="", flush=True)
+    ]
+) as stream:
+    for chunk in stream:
+        if chunk.type == "content.delta":
+            print(chunk.delta, end="", flush=True)
