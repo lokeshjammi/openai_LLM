a3089828fdd618532317e92f4643869fa9a3ec02
lokeshjammi <jammilokesh3@gmail.com>
2025-11-13 23:09:11 +0530

    Hugging Face code commit

---
+++ hugging_face_learn.py
@@ -0,0 +1,27 @@
+from dotenv import load_dotenv
+import os
+from openai import OpenAI
+
+load_dotenv(".env")
+client = OpenAI(
+    base_url="https://router.huggingface.co/v1",
+    api_key=os.getenv("HUGGING_FACE_API_KEY"),
+)
+
+messages = [
+    {
+        "role": "system",
+        "content": "You are a helpful assistant.",
+    },
+    {
+        "role": "user",
+        "content": "Explain the theory of relativity in simple terms.",
+    },
+]
+
+response = client.chat.completions.create(
+    model="meta-llama/Llama-3.1-8B-Instruct",
+    messages=messages
+)
+
+print(response.choices[0].message.content)
