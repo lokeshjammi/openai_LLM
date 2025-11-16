8b4f7405db5b049ea3b9154cda27faaae2ba9237
lokeshjammi <jammilokesh3@gmail.com>
2025-11-16 09:58:06 +0530

    Transformers code commit

--- hugging_face_inference_learn.py
+++ hugging_face_inference_learn.py
@@ -1,27 +1,38 @@
 from dotenv import load_dotenv
 import os
 from openai import OpenAI
+from transformers import pipeline
+
 
 load_dotenv(".env")
 client = OpenAI(
     base_url="https://router.huggingface.co/v1",
     api_key=os.getenv("HUGGING_FACE_API_KEY"),
 )
+def learn_inference():
+    messages = [
+        {
+            "role": "system",
+            "content": "You are a helpful assistant.",
+        },
+        {
+            "role": "user",
+            "content": "Explain the theory of relativity in simple terms.",
+        },
+    ]
 
-messages = [
-    {
-        "role": "system",
-        "content": "You are a helpful assistant.",
-    },
-    {
-        "role": "user",
-        "content": "Explain the theory of relativity in simple terms.",
-    },
-]
+    response = client.chat.completions.create(
+        model="meta-llama/Llama-3.1-8B-Instruct",
+        messages=messages
+    )
 
-response = client.chat.completions.create(
-    model="meta-llama/Llama-3.1-8B-Instruct",
-    messages=messages
-)
+    print(response.choices[0].message.content)
 
-print(response.choices[0].message.content)
+def learn_pipeline():
+    pipe = pipeline(
+        task='text-generation',
+        model='openai-community/gpt2'
+    )
+    print(pipe("Can you explain me how pipeline works?"))
+
+learn_pipeline()
