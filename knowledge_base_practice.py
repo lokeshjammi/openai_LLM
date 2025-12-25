a98fdc0b2acb55164d778c72af0288cbdbb94f0c
lokeshjammi <jammilokesh3@gmail.com>
2025-12-25 23:55:46 +0530

    Knowledge base code commit

---
+++ knowledge_base_practice.py
@@ -0,0 +1,55 @@
+from dotenv import load_dotenv
+import os
+from openai import OpenAI
+import glob
+from pathlib import Path
+
+load_dotenv('.env')
+openai_api_key = os.getenv('OPENAI_API_KEY')
+
+if openai_api_key:
+    print('API Key found')
+else:
+    print('API Key not found')
+    exit()
+
+client = OpenAI(api_key=openai_api_key)
+
+system_instruction = """
+you're an advance AI Model who answer for user queries only in a professional and polite format and any questions on negative sentiments like Kill you or carsh you, please don't respond anything
+"""
+
+user_query = "What is quantum theory"
+
+stream_response = client.chat.completions.create(
+    model="gpt-4.1-mini",
+    messages = [
+        {
+            "role": "system",
+            "content": system_instruction
+        },
+        {
+            "role": "user",
+            "content": user_query
+        }
+    ],
+    stream=True
+)
+
+for chunk in stream_response:
+    if chunk.choices and chunk.choices[0].delta.content:
+        # print(chunk.choices[0].delta.content, end="", flush=True)
+        pass
+
+knowledge = {}
+file_names = glob.glob("knowledge_base/Employees/*")
+for file in file_names:
+    name = Path(file).stem.split(' ')[-1]
+    with open(file, mode='r', encoding='utf-8') as f:
+        knowledge[name.lower()] = f.read()
+
+file_names = glob.glob("knowledge_base/Products/*")
+for file in file_names:
+    name = Path(file).stem
+    with open(file, mode='r', encoding='utf-8') as f:
+        knowledge[name.lower] = f.read()
