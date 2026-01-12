b772fb6af710277221723324ab16fafd80b0f18a
lokeshjammi <jammilokesh3@gmail.com>
2026-01-12 21:26:31 +0530

    RAG Pipeline code changes commit

--- rag_pipeline.py
+++ rag_pipeline.py
@@ -3,8 +3,11 @@ import os
 from langchain_huggingface import HuggingFaceEmbeddings
 from openai import OpenAI
 from langchain_chroma import Chroma
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import SystemMessage, HumanMessage
 
-model = "gpt-4.1-mini"
+os.system('clear')
+MODEL = "gpt-4.1-nano"
 db_name = "vector_db"
 load_dotenv('.env')
 
@@ -20,3 +23,24 @@ client = OpenAI(api_key=openai_api_key)
 embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2", model_kwargs = {"device": "cpu"})
 vector_store = Chroma(persist_directory=db_name, embedding_function=embeddings)
 
+retriever = vector_store.as_retriever()
+llm = ChatOpenAI(model=MODEL, temperature=0.2)
+
+System_instruction = """
+You are a helpful assistant who represent a company called insuretech that can answer questions about the documents in the knowledge base.
+if relavent use the context to answer the question.
+"""
+
+def answer_question(query):
+    docs = retriever.invoke(query)
+    print(len(docs))
+    context = ""
+    for doc in docs:
+        context += doc.page_content
+    # print(context)
+    system_prompt = System_instruction+"\n\nContext: "+context
+    # print(system_prompt)
+    response = llm.invoke([SystemMessage(content=system_prompt), HumanMessage(content=query)])
+    # print(response.content)
+
+answer_question("Who is carter?")
