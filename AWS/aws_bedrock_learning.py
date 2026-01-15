87281b9ba8d412640c921d6bab3c38a23f47f877
lokeshjammi <jammilokesh3@gmail.com>
2026-01-15 20:41:07 +0530

    AWS Haiku model code commit

---
+++ AWS/aws_bedrock_learning.py
@@ -0,0 +1,25 @@
+import boto3
+import json
+
+client = boto3.client(service_name="bedrock-runtime", region_name="us-east-2")
+model_id = "anthropic.claude-haiku-4-5-20251001-v1:0"
+
+body = json.dumps({
+    "anthropic_version": "bedrock-2023-05-31",
+    "max_tokens": 500,
+    "messages": [
+        {
+            "role": "user",
+            "content": "Hello! Can you tell me a short joke?"
+        }
+    ]
+})
+
+response = client.invoke_model(
+    modelId=model_id,
+    body=body,
+    accept="application/json",
+    contentType="application/json"
+)
+
+print(response)
